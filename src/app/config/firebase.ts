9b8c618b35deede0d367cd312116ec9dffbec524
topcoder917 <snowgirl1991718@hotmail.com>
2020-12-22 22:54:55 +0800

    2020/12/22-Added config file

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,16 @@
+export const firebaseconfig = {
+
+    firebase : {
+      apiKey: "AIzaSyA9DKL-87d1slz8-N32-ChHkIVddI14fjg",
+      authDomain: "vx-power.firebaseapp.com",
+      databaseURL: "https://vx-power.firebaseio.com",
+      projectId: "vx-power",
+      storageBucket: "vx-power.appspot.com",
+      messagingSenderId: "56506828040",
+      appId: "1:56506828040:web:f87e4945c499ded8eb58bd",
+      measurementId: "G-872SS4G5VY"
+
+      }
+
+
+};
